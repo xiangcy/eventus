e917e85665fdd76c447c6a805a539292df17a2d2
Chongyuan <xiangcy@mit.edu>
2013-01-11 21:14:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XDLRepo::Application.config.secret_token = 'b3bab0581c397cc6f8a4872f297ec89daeb8760092273504bc7c12d3094fcfd8da7f7c9d20800288321e8342ac649eebe644ad4e32d0e4a65f1dc906fc1ba259'
