43f8ad586cde2b6fe051521ac565f408591155ee
Chongyuan <xiangcy@mit.edu>
2013-01-14 02:33:50 -0500

    "Finish sign in"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 XDLRepo::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get "static_pages/home"
   get "static_pages/about"
   
   root to: 'static_pages#home'
   match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/about', to: 'static_pages#about'
   
   # The priority is based upon order of creation:
