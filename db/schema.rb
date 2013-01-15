44b3b7df4df61468fd9fa2025f00d1abf8ee5109
Xinyue Deng <dxy0420@mit.edu>
2013-01-15 00:22:50 -0500

    modify layouts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114060254) do
+ActiveRecord::Schema.define(:version => 20130114221456) do
+
+  create_table "events", :force => true do |t|
+    t.string   "time"
+    t.string   "place"
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
