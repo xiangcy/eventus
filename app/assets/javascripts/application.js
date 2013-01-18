7edd78d14897fd62498c869a29c2207964f02f87
Xinyue Deng <dxy0420@mit.edu>
2013-01-18 00:24:23 -0500

    reset

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,4 @@ function edit(){
     $(".edit").html("<p><font color = #B3B3B3>Edit</font></p>");
     $("table").append("<input type='submit'value='Save' class='btn btn-primary'><input type='submit'value='Cancel' class='btn'>");
 }
+
