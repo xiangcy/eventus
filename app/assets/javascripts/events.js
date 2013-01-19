29746e171f9426815436f6a23154ed67669a7fcf
kevin-li <wli2012@mit.edu>
2013-01-19 01:00:59 -0500

    eventstyle

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,13 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$(document).ready(function(){
+	
+	var mapOptions = {
+	  center: new google.maps.LatLng(-34.397, 150.644),
+	  zoom: 8,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+}
