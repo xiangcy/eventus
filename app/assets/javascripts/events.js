7679e54ec8c88798025eab585ce7dd7e343b95fb
kevin-li <wli2012@mit.edu>
2013-01-19 18:29:58 -0500

    postnew/edit

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,10 +3,7 @@
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 $(document).ready(function(){
-	var mapOptions = {
-	  center: new google.maps.LatLng(-34.397, 150.644),
-	  zoom: 8,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+
+	
+
 }
