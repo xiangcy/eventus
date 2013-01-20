956c717963eff76ae25f3ffce5ef7121a7a58470
kevin-li <wli2012@mit.edu>
2013-01-19 21:41:16 -0500

    asdfasdfasdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+//= require jquery-ui
 
 function edit(){
     $(".content").each(function(){
--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,22 +1,7 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-
-$(document).ready(
-	alert("adfasdf");
-	 $(function() {
-    $('#datetimepicker1').datetimepicker({
-		format: 'dd/MM/yyyy hh:mm',
-      language: 'en'
-    });
-
-    $('#datetimepicker2').datetimepicker({
-		format: 'dd/MM/yyyy hh:mm',
-      language: 'en'
-    });
-  });
-
-
-
-
-}
+//# Place all the behaviors and hooks related to the matching controller here.
+//# All this logic will automatically be available in application.js.
+//# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+	
+$(document).ready(function(){
+	$('#selectedDateTime').datetimepicker();
+});
