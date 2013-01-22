d15fe61506305bbd1cc92161107d3a875958c8f4
Xinyue Deng <dxy0420@mit.edu>
2013-01-22 07:28:50 -0500

    mofify edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(:version => 20130121001253) do
     t.string   "category"
   end
 
-  add_index "events", ["user_id", "created_at"], :name => "index_events_on_user_id_and_created_at"
-
   create_table "rs_evaluations", :force => true do |t|
     t.string   "reputation_name"
     t.integer  "source_id"
