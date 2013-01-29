e14eec26e16d13880e18d319043181a6a9ad04f8
kevin-li <wli2012@mit.edu>
2013-01-29 02:57:26 -0500

    minor fixes

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,16 @@
 
+
+
+$(document).ready(function(){
+
+    
+    $( ".tabs" ).tabs({ cookie: { expires: 30 } });
+
+    var cookie = $( ".selector" ).tabs( "option", "cookie" );
+  
+    $( ".tabs" ).tabs( "option", "cookie", { expires: 30 } )
+    
+});
+
+
+
